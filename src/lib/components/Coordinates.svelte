5574c8c547a90afa369a7586c41b5839c36b056f
XtratoS <28851891+XtratoS@users.noreply.github.com>
2024-07-04 12:25:41 +0300

    Remove console.log

--- src/lib/components/Coordinates.svelte
+++ src/lib/components/Coordinates.svelte
@@ -4,7 +4,6 @@
   export let smallScreen: boolean;
 
   const approximateNumber = (value: number) => {
-    console.log("approximateNumber");
     return (Math.round(value * 1000) / 1000).toFixed(3);
   }
 </script>
