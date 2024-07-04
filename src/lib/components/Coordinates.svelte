da93ddfb4170861438439af581943dafee084fa4
XtratoS <28851891+XtratoS@users.noreply.github.com>
2024-07-04 11:36:14 +0300

    Create Coordinates component to show pin coordinates on top right

---
+++ src/lib/components/Coordinates.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { markerLocation } from "$lib/stores";
+  
+  export let smallScreen: boolean;
+
+  const approximateNumber = (value: number) => {
+    console.log("approximateNumber");
+    return (Math.round(value * 1000) / 1000).toFixed(3);
+  }
+</script>
+
+<div class="coordinates-holder">
+  {#if smallScreen}
+    <div class="coordinate"><span>Lat:</span><span>{approximateNumber($markerLocation.lat)}</span></div>
+    <div class="coordinate"><span>Long:</span><span>{approximateNumber($markerLocation.lng)}</span></div>
+  {:else}
+    <div class="coordinate"><span>Latitude:</span><span>{$markerLocation.lat.toFixed(15)}</span></div>
+    <div class="coordinate"><span>Longitude:</span><span>{$markerLocation.lng.toFixed(15)}</span></div>
+  {/if}
+</div>
+
+<style>
+  .coordinates-holder {
+    position: absolute;
+    top: .5rem;
+    right: .5rem;
+    padding: .3rem;
+    display: flex;
+    flex-direction: column;
+    background-color: rgba(255, 255, 255, 0.5);
+  }
+  .coordinate {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 0.5rem;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+  import Coordinates from "$lib/components/Coordinates.svelte";
   import Map from "$lib/components/Map.svelte";
+
+  let smallScreen = window.matchMedia("(max-width: 768px)").matches;
+  const onResize = () => {
+    smallScreen = window.matchMedia("(max-width: 768px)").matches;
+  }
 </script>
 
+<svelte:window on:resize={onResize} />
+
 <Map />
+<Coordinates bind:smallScreen />
